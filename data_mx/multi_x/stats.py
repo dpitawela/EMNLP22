e1b3687d6eb58378a3bea2ea32af813e0c5ee60d
Dileepa Pitawela <dzoneimaginez@gmail.com>
2022-07-11 15:49:38 +0930

    cleaning

--- data_mx/multi_x/stats.py
+++
@@ -1,8 +0,0 @@
-# golden summaries list
-with open("tokTrunc_1024_utf/trainY.txt", "r", encoding="utf-8") as f:
-    ref = [line.strip()[2:] for line in f]  # removing the dash in the beginning and making the list
-
-print("No. data", len(ref))
-print("Ref-avg-len", sum(map(len, [sent.split(' ') for sent in ref])) / len(ref))
-print("Ref-max-len", len(max([sent.split(' ') for sent in ref], key=len)))
-print("Ref-min-len", len(min([sent.split(' ') for sent in ref], key=len)), "\n")
